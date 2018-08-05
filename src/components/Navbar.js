a5e814ed36eb0cdc614490e3b1697836a8f904e0
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-08-05 21:53:12 +0100

    remove old logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,13 @@ import Link from 'gatsby-link'
 import Icon from './Icon'
 import github from '../img/github-icon.svg'
 import twitter from '../img/twitter-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          Home
         </Link>
       </div>
       <div className="navbar-start">
