96d5ed0625f34e1a76524931b16e9a444f931a36
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-08-06 16:50:43 +0100

    blog post typos

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
+import Content, { HTMLContent } from '../components/Content';
 import { Main, Container, Title, Opener, Subtitle, Meta, List, ListItem } from '../styled-components';
 
 export const BlogPostTemplate = ({
@@ -14,32 +14,32 @@ export const BlogPostTemplate = ({
   title,
   helmet,
 }) => {
-  const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content;
 
   return (
     <Main>
       {helmet || ''}
       <Container>
-            <Title>
-              {title}
-            </Title>
-            <Opener>{description}</Opener>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <Meta>
-                <Subtitle>Tags</Subtitle>
-                <List>
-                  {tags.map(tag => (
-                    <ListItem key={tag}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </ListItem>
-                  ))}
-                </List>
-              </Meta>
-            ) : null}
+        <Title>
+            {title}
+        </Title>
+        <Opener>{description}</Opener>
+        <PostContent content={content} />
+        {tags && tags.length ? (
+            <Meta>
+            <Subtitle>Tags</Subtitle>
+            <List>
+                {tags.map(tag => (
+                <ListItem key={tag}>
+                    <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </ListItem>
+                ))}
+            </List>
+            </Meta>
+        ) : null}
       </Container>
     </Main>
-  )
+  );
 }
 
 BlogPostTemplate.propTypes = {
@@ -51,7 +51,7 @@ BlogPostTemplate.propTypes = {
 }
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <BlogPostTemplate
@@ -68,7 +68,7 @@ const BlogPost = ({ data }) => {
 BlogPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
-  }),
+  })
 }
 
 export default BlogPost
