7a90ca232910e2d6cfc3fdf76ad33b12af9540c2
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-08-11 00:11:52 +0100

    tag sorting set up in render

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -4,24 +4,46 @@ import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import { Main, Content, Title, List, ListItem, Opener, Option } from '../../styled-components';
 
-const TagsPage = ({data}) => {
-    const {title} = data.site.siteMetadata;
-    const {group} = data.allMarkdownRemark;
-    return <Main>
-        <Helmet title={`Tags | ${title}`} />
-        <Content>
-            <Title>Tags</Title>
-            <List>
-                {group.map(tag => (
-                    <ListItem key={tag.fieldValue}>
-                        <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                            <Option>{tag.fieldValue} ({tag.totalCount})</Option>
-                        </Link>
-                    </ListItem>
-                ))}
-            </List>
-        </Content>
-    </Main>
+class TagsPage extends React.Component {
+    state = {
+        sortBy : 'use'
+    }
+
+    render () {
+        const {data} = this.props;
+        const {title} = data.site.siteMetadata;
+        const {group} = data.allMarkdownRemark;
+        const {sortBy} = this.state;
+    
+        group.sort((a, b) => {
+            if (sortBy === 'use') {
+                return b.totalCount - a.totalCount;
+            } else {
+                if(a.fieldValue.toLowerCase() < b.fieldValue.toLowerCase()) return -1;
+                if(a.fieldValue.toLowerCase() > b.fieldValue.toLowerCase()) return 1;
+                return 0;
+            }
+        });
+
+        console.log(group);
+    
+        return <Main>
+            <Helmet title={`Tags | ${title}`} />
+            <Content>
+                <Title>Tags</Title>
+                <List>
+                    {group.map(tag => (
+                        <ListItem key={tag.fieldValue}>
+                            <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                                <Option>{tag.fieldValue} ({tag.totalCount})</Option>
+                            </Link>
+                        </ListItem>
+                    ))}
+                </List>
+            </Content>
+        </Main>
+
+    }
 }
 
 export default TagsPage
