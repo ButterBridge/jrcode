ccf060cde945d2c5a5b4d449b0796663645ef960
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-08-18 00:07:43 +0100

    format Transition.js

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -4,57 +4,53 @@ import getTransitionStyle from "../utils/transitions"
 import { historyExitingEventType, timeout } from "../../gatsby-browser"
 
 class Transition extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { exiting: false }
-    this.listenerHandler = this.listenerHandler.bind(this)
-  }
-
-  listenerHandler(event) {
-    this.setState({ exiting: true })
-  }
-
-  componentDidMount() {
-    window.addEventListener(historyExitingEventType, this.listenerHandler)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(historyExitingEventType, this.listenerHandler)
-  }
-
-  static getDerivedStateFromProps({ exiting }) {
-    if (exiting) {
-      return { exiting: false }
-    }
-    return null
-  }
-
-  render() {
-    const transitionProps = {
-      timeout: {
-        enter: this.props.additionalTimeout || 0,
-        exit: timeout,
-      },
-      appear: true,
-      in: !this.state.exiting,
+    state = { 
+        exiting: false
     }
 
-    console.log(transitionProps);
+    listenerHandler = (event) => {
+        this.setState({ exiting: true })
+    }
 
-    return (
-      <ReactTransition {...transitionProps}>
-        {status => (
-          <div
-            style={{
-              ...getTransitionStyle({ status, timeout }),
-            }}
-          >
-            {this.props.children}
-          </div>
-        )}
-      </ReactTransition>
-    )
-  }
+    componentDidMount() {
+        window.addEventListener(historyExitingEventType, this.listenerHandler)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener(historyExitingEventType, this.listenerHandler)
+    }
+
+    static getDerivedStateFromProps({ exiting }) {
+        if (exiting) {
+            return { exiting: false }
+        }
+        return null
+    }
+
+    render() {
+        const transitionProps = {
+            timeout: {
+                enter: this.props.additionalTimeout || 0,
+                exit: timeout,
+            },
+            appear: true,
+            in: !this.state.exiting,
+        }
+
+        return (
+            <ReactTransition {...transitionProps}>
+                {status => (
+                    <div
+                        style={{
+                            ...getTransitionStyle({ status, timeout }),
+                        }}
+                    >
+                        {this.props.children}
+                    </div>
+                )}
+            </ReactTransition>
+        )
+    }
 }
 
-export default Transition
+export default Transition;
