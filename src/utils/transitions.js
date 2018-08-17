2900fe5a5d7ec66a83c0462ecf1a6bcaede8dca1
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-08-18 00:09:16 +0100

    format getTransitionStyles

--- src/utils/transitions.js
+++ src/utils/transitions.js
@@ -1,20 +1,19 @@
 const getTransitionStyles = timeout => {
     return {
-      entering: {
-        opacity: 0,
-      },
-      entered: {
-        transition: `opacity ${timeout}ms ease-in-out`,
-        opacity: 1,
-      },
-      exiting: {
-        transition: `opacity ${timeout}ms ease-in-out`,
-        opacity: 0,
-      },
+        entering: {
+            opacity: 0,
+        },
+        entered: {
+            transition: `opacity ${timeout}ms ease-in-out`,
+            opacity: 1,
+        },
+        exiting: {
+            transition: `opacity ${timeout}ms ease-in-out`,
+            opacity: 0,
+        },
     }
-  }
+}
   
-const getTransitionStyle = ({ timeout, status }) =>
-    getTransitionStyles(timeout)[status]
+const getTransitionStyle = ({ timeout, status }) => getTransitionStyles(timeout)[status];
   
-export default getTransitionStyle
+export default getTransitionStyle;
