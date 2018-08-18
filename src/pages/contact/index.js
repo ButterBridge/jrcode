d7a953b8d37b30003998336573ad3b58bf1a93c8
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-08-18 20:55:15 +0100

    refactor form component

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,98 +1,122 @@
 import React from "react";
-import { navigateTo } from "gatsby-link";
-import {Main, TransitionContainer, Meta, FormLabel, FormTextarea, FormInput, FormButton} from '../../styled-components';
-
-const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
+import {Main, TransitionContainer, Meta, FormLabel, FormTextarea, FormInput, FormButton, Opener} from '../../styled-components';
+import { encode } from "../../utils/helpers";
 
 export default class Contact extends React.Component {
-state = { name: "", email: "", message: "" };
+    state = { 
+        name: '',
+        email: '',
+        message: '',
+        formSent: false,
+        formSendError: false,
+        submitting: false
+    };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    render() {
+        const {formSent, formSendError, submitting} = this.state;
+        return (
+            <Main>
+                <TransitionContainer>
+                    {formSendError && <Opener>There was an error sending your message... please try again later.</Opener>}
+                    {formSent && <Opener>Thanks for your message! I'll get back to you as soon as possible.</Opener>}
+                    <form name="contact" method="post" data-netlify="true" className="grid-form">
+                        <input type="hidden" name="form-name" value="contact" />
+                        <FormLabel
+                            gridArea={{
+                                from : '1 / 1',
+                                to: '2 / 2'
+                            }}
+                        >Name please!</FormLabel>
+                        <FormInput 
+                            type="text"
+                            name="name"
+                            gridArea={{
+                                from : '1 / 2',
+                                to: '2 / 3'
+                            }}
+                        />
+                        <FormLabel
+                            gridArea={{
+                                from : '2 / 1',
+                                to: '3 / 2'
+                            }}
+                        >Contact email:</FormLabel>
+                        <FormInput 
+                            type="email"
+                            name="email"
+                            gridArea={{
+                                from : '2 / 2',
+                                to: '3 / 3'
+                            }}
+                        />
+                        <FormLabel
+                            gridArea={{
+                                from : '3 / 1',
+                                to: '4 / 2'
+                            }}
+                        >What you got?</FormLabel>
+                        <FormTextarea
+                            name="message"
+                            gridArea={{
+                                from : '3 / 2',
+                                to: '4 / 3'
+                            }}    
+                        />
+                        <FormButton
+                            onClick={this.handleSubmit}
+                            disabled={submitting}
+                            gridArea={{
+                                from : '4 / 1',
+                                to: '5 / 3'
+                            }} 
+                        >
+                            Send
+                        </FormButton>
+                    </form>
+                </TransitionContainer>
+            </Main>
+        );
+    }
 
 
+    handleChange = e => {
+        this.setState({ 
+            [e.target.name]: e.target.value 
+        });
+    };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
-    })
-      .then(() => navigateTo(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
+    handleSubmit = e => {
+        e.preventDefault();
+        const {name, email, message} = this.state;
 
-  render() {
-    return (
-        <Main>
-            <TransitionContainer>
-                <form name="contact" method="post" data-netlify="true" className="grid-form">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <FormLabel
-                        gridArea={{
-                            from : '1 / 1',
-                            to: '2 / 2'
-                        }}
-                    >Name please!</FormLabel>
-                    <FormInput 
-                        type="text"
-                        name="name"
-                        gridArea={{
-                            from : '1 / 2',
-                            to: '2 / 3'
-                        }}
-                    />
-                    <FormLabel
-                        gridArea={{
-                            from : '2 / 1',
-                            to: '3 / 2'
-                        }}
-                    >Contact email:</FormLabel>
-                    <FormInput 
-                        type="email"
-                        name="email"
-                        gridArea={{
-                            from : '2 / 2',
-                            to: '3 / 3'
-                        }}
-                    />
-                    <FormLabel
-                        gridArea={{
-                            from : '3 / 1',
-                            to: '4 / 2'
-                        }}
-                    >What you got?</FormLabel>
-                    <FormTextarea
-                        name="message"
-                        gridArea={{
-                            from : '3 / 2',
-                            to: '4 / 3'
-                        }}    
-                    />
-                    <FormButton 
-                        type="submit"
-                        gridArea={{
-                            from : '4 / 1',
-                            to: '5 / 3'
-                        }} 
-                    >
-                        Send
-                    </FormButton>
-                </form>
-            </TransitionContainer>
-        </Main>
-    );
-  }
+        this.setState({
+            formSent: false,
+            formSendError: false,
+            submitting: true
+        })
+
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': e.target.name,
+                name, email, message
+            })
+        })
+        .then(() => {
+            this.setState({
+                formSent: true,
+                name: '',
+                email: '',
+                message: '',
+                submitting: false
+            })
+        })
+        .catch(() => 
+            this.setState({
+                formSendError: true,
+                submitting: false
+            })
+        );
+    };
 }
