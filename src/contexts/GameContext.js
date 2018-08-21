9fb82a88e379247bba133ed7d9b6bbf78aa21fff
Jonathan Rathbone <jontyr@jonathan-rathbones-computer.local>
2018-08-21 22:06:16 +0100

    make round 1 of game completable

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {colours} from '../style';
-import {sample} from 'lodash';
+import {sample, difference} from 'lodash';
 import * as helpers from '../utils/helpers';
 
 export const GameContext = React.createContext('hi');
@@ -8,14 +8,19 @@ export const GameContext = React.createContext('hi');
 export class GameProvider extends React.Component {
     state = {
         colours: [],
-        round : '1',
+        progressing: false,
+        round : 1,
         '1' : {
             onMouseOverHeadLetter : 'swapColourRandomly',
-            checkResult : 'areAllEqual'
+            checkResult : 'areAllEqual',
+            onPositiveResult : 'targetNewColour',
+            checkCompletion : 'areEquivalent',
+            progress : []
         }
     }
 
     render () {
+        console.log(this.state.round)
         return (
             <GameContext.Provider value={{
                 colours : this.state.colours,
@@ -34,24 +39,52 @@ export class GameProvider extends React.Component {
     }
 
     onMouseOverHeadLetter = letterIndex => {
-        const round = this.state[this.state.round];
+        let round = this.state[this.state.round];
         this[round.onMouseOverHeadLetter](letterIndex, () => {
             if (helpers[round.checkResult](this.state.colours)) {
-                console.log('done!')
-            } else {
-                console.log('not yet!')
+                console.log('check good!');
+                this[round.onPositiveResult](round, () => {
+                    round = this.state[this.state.round];
+                    if (helpers[round.checkCompletion](round.progress, colours)) {
+                        this.progressToNextRound();
+                    }
+                });
             }
         });
     }
 
+    progressToNextRound = () => {
+        this.setState({
+            round : this.state.round + 1
+        })
+    }
+
     swapColourRandomly = (targetIndex, done) => {
         this.setState({
-            colours : this.state.colours.map((font, index) => {
+            colours : this.state.colours.map((colour, index) => {
                 return index === targetIndex ?
                     sample(colours) :
-                    font
+                    colour
             })
-        }, done)
+        }, () => {
+            console.log(this.state.colours);
+            done();
+        })
+    }
+
+    targetNewColour = ({progress}, done) => {
+        const {colours : currentColours, round} = this.state;
+        this.setState({
+            colours : currentColours.map((colour, index) => {
+                return index === currentColours.length - 1 ? 
+                    sample(difference(colours, [...progress, currentColours[0]])) :
+                    sample(colours)
+            }),
+            [round] : {
+                ...this.state[round],
+                progress : [...progress, currentColours[0]]
+            }
+        }, done)        
     }
 
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
+import {difference} from 'lodash';
+
 export const areAllEqual = array => {
     return array.every(item => item === array[0]);
+}
+
+export const areEquivalent = (array1, array2) => {
+    return !difference(array2, array1).length
 }
