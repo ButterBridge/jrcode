422ca18f9e274df95c3b53097c06a6082be916f7
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-09-09 09:52:08 +0100

    fix brand proptypes

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -5,74 +5,75 @@ import { window } from 'browser-monads';
 import Sidebar from './canvases/Sidebar';
 import Transition from './Transition';
 import { HeadLetter } from '../styled-components';
-import {fonts} from '../style';
+import { fonts } from '../style';
 
 class Brand extends Component {
-    state = {
-        fonts : Array(this.props.siteName.length + 1).fill().map(x => sample(fonts)),
-    }
+  state = {
+    fonts: Array(this.props.siteName.length + 1).fill().map(x => sample(fonts)),
+  }
 
-    componentDidMount = () => {
-        this.props.generateRandomColours(this.props.siteName.length + 1);
-    }
+  componentDidMount = () => {
+    this.props.generateRandomColours(this.props.siteName.length + 1);
+  }
 
-    render() {
-        const {fonts} = this.state;
-        const {siteName, isSmall, isAlone, colours, onMouseOverHeadLetter, progressing} = this.props;
-        if (!colours.length) return null;
-        const adjustedColours = isSmall ? ['', ...colours.slice(0, -1)]: colours;
+  render() {
+    const { fonts } = this.state;
+    const { siteName, isSmall, isAlone, colours, onMouseOverHeadLetter, progressing } = this.props;
+    if (!colours.length) return null;
+    const adjustedColours = isSmall ? ['', ...colours.slice(0, -1)] : colours;
 
-        return (
-            <div className={`grid-sidebar${isSmall ? '-mini' : ''}`}>
-                {`${isSmall ? ' ' : ''}${siteName}`.split('').map((char, i) => {
-                    return <HeadLetter
-                        gridCols={{from : isSmall ? 1 : i + 1, to : isSmall ? 2 : i + 2}}
-                        key={i}
-                        font={fonts[i]}
-                        colour={adjustedColours[i]}
-                        onMouseEnter={() => {
-                            this.changeFont(i);
-                            !progressing && onMouseOverHeadLetter(isSmall ? i - 1 : i);
-                        }}
-                    >
-                        {char}
-                    </HeadLetter>
-                })}
-                {!isSmall && !isAlone && <div className="grid-sidebar-canvas">
-                    {window.hasOwnProperty('Pts') && <Transition
-                        additionalTimeout={600}
-                        actions={['descend']}
-                        style={{width : '100%', height : '100%'}}
-                    >
-                        <Sidebar
-                            colours={colours}
-                            dimensions={[siteName.length, 13]}
-                            progressing={progressing}
-                        />
-                    </Transition>}
-                </div>}
-            </div> 
-        );
-    }
+    return (
+      <div className={`grid-sidebar${isSmall ? '-mini' : ''}`}>
+        {`${isSmall ? ' ' : ''}${siteName}`.split('').map((char, i) => {
+          return <HeadLetter
+            gridCols={{ from: isSmall ? 1 : i + 1, to: isSmall ? 2 : i + 2 }}
+            key={i}
+            font={fonts[i]}
+            colour={adjustedColours[i]}
+            onMouseEnter={() => {
+              this.changeFont(i);
+              !progressing && onMouseOverHeadLetter(isSmall ? i - 1 : i);
+            }}
+          >
+            {char}
+          </HeadLetter>
+        })}
+        {!isSmall && !isAlone && <div className="grid-sidebar-canvas">
+          {window.hasOwnProperty('Pts') && <Transition
+            additionalTimeout={600}
+            actions={['descend']}
+            style={{ width: '100%', height: '100%' }}
+          >
+            <Sidebar
+              colours={colours}
+              dimensions={[siteName.length, 13]}
+              progressing={progressing}
+            />
+          </Transition>}
+        </div>}
+      </div>
+    );
+  }
 
-    changeFont = targetindex => {
-        this.setState({
-            fonts : this.state.fonts.map((font, index) => {
-                return index === targetindex ?
-                    sample(fonts) :
-                    font
-            })
-        })
-    }
+  changeFont = targetindex => {
+    this.setState({
+      fonts: this.state.fonts.map((font, index) => {
+        return index === targetindex ?
+          sample(fonts) :
+          font
+      })
+    })
+  }
 }
 
 Brand.propTypes = {
-    siteName : PT.string.isRequired,
-    isSmall : PT.bool.isRequired,
-    isAlone : PT.bool,
-    colours : PT.arrayOf(PT.string).isRequired,
-    onMouseOverHeadLetter : PT.func.isRequired,
-    generateRandomColours : PT.func.isRequired
+  siteName: PT.string.isRequired,
+  isSmall: PT.bool.isRequired,
+  isAlone: PT.bool,
+  colours: PT.arrayOf(PT.string).isRequired,
+  onMouseOverHeadLetter: PT.func.isRequired,
+  generateRandomColours: PT.func.isRequired,
+  progressing: PT.bool.isRequired
 }
 
 export default Brand;
