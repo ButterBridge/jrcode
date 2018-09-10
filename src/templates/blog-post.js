685c179cc2972f6aa6e490a67e25a60339b46ecd
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-09-10 07:50:29 +0100

    add lowercase sorting on blog post template & format file

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,134 +9,128 @@ import { Main, BulletedTitle, Opener, Subtitle, Meta, List, LinkedListItem, Cent
 import { GameContext } from '../contexts/GameContext';
 
 export const BlogPostTemplate = ({
-    content,
-    contentComponent : PostContent = Content,
-    siteTitle,
-    images,
-    frontmatter : {description, tags, title, caption, date, formattedDate}
+  content,
+  contentComponent: PostContent = Content,
+  siteTitle,
+  images,
+  frontmatter: { description, tags, title, caption, date, formattedDate }
 }) => {
 
-    tags.sort((a, b) => {
-        if(a < b) return -1;
-        if(a > b) return 1;
-        return 0;
-    })
+  tags.sort((a, b) => {
+    if (a.toLowerCase() < b.toLowerCase()) return -1;
+    if (a.toLowerCase() > b.toLowerCase()) return 1;
+    return 0;
+  })
 
-    return (
-        <GameContext.Consumer>
-            {({colours}) => {
-                if (!colours.length) return null;
-                return <Main
-                    colour={colours[siteTitle.length]}
-                >
-                    <Helmet title={`${siteTitle} - blog - ${title}`} />
-                    <TransitionContainer>
-                        <BulletedTitle
-                            addition="title"
-                            componentContent={title}
-                            colour={colours[siteTitle.length]}
-                            classTag={'post'}
-                        />
-                        <Meta
-                            colour={colours[siteTitle.length]}
-                        >
-                            <Detail>
-                                {formattedDate}
-                            </Detail>
-                        </Meta>
-                        <Subtitle>{description}</Subtitle>
-                        <PostContent content={content} />
-                        {images && <Centraliser>
-                            <Image 
-                                images={images}
-                                caption={caption}
-                                colour={colours[siteTitle.length] || colours[0]}
-                            />
-                        </Centraliser>}
-                        {tags && tags.length && (
-                            <Meta
-                                colour={colours[siteTitle.length]}
-                            >
-                                <Subtitle>Tags</Subtitle>
-                                <List>
-                                    {tags.map(tag => (
-                                    <LinkedListItem
-                                        key={tag}
-                                        colour={colours[siteTitle.length]}
-                                        linkTo={`/tags/${kebabCase(tag)}/`}
-                                    >
-                                        <Option>{tag}</Option>
-                                    </LinkedListItem>
-                                    ))}
-                                </List>
-                            </Meta>
-                        )}
-                    </TransitionContainer>
-                </Main>
-            }}
-        </GameContext.Consumer>
-    );
+  
+  return (
+    <GameContext.Consumer>
+      {({ colours }) => {
+      const themeColour = colours[siteTitle.length];
+        if (!colours.length) return null;
+        return <Main colour={themeColour}>
+          <Helmet title={`${siteTitle} - blog - ${title}`} />
+          <TransitionContainer>
+            <BulletedTitle
+              addition="title"
+              componentContent={title}
+              colour={themeColour}
+              classTag={'post'}
+            />
+            <Meta colour={themeColour}>
+              <Detail>{formattedDate}</Detail>
+            </Meta>
+            <Subtitle>{description}</Subtitle>
+            <PostContent content={content} />
+            {images && <Centraliser>
+              <Image
+                images={images}
+                caption={caption}
+                colour={themeColour || colours[0]}
+              />
+            </Centraliser>}
+            {tags && tags.length && (
+              <Meta colour={themeColour}>
+                <Subtitle>Tags</Subtitle>
+                <List>
+                  {tags.map(tag => (
+                    <LinkedListItem
+                      key={tag}
+                      colour={themeColour}
+                      linkTo={`/tags/${kebabCase(tag)}/`}
+                    >
+                      <Option>{tag}</Option>
+                    </LinkedListItem>
+                  ))}
+                </List>
+              </Meta>
+            )}
+          </TransitionContainer>
+        </Main>
+      }}
+    </GameContext.Consumer>
+  );
 }
 
 BlogPostTemplate.propTypes = {
-    content: PropTypes.string.isRequired,
-    contentComponent: PropTypes.func,
-    frontmatter: PropTypes.object.isRequired,
-    helmet: PropTypes.object,
+  content: PropTypes.string.isRequired,
+  contentComponent: PropTypes.func,
+  frontmatter: PropTypes.object.isRequired,
+  helmet: PropTypes.object,
 }
 
-const BlogPost = ({data}) => {
-    const { markdownRemark: post, allFile : {edges}} = data;
-    const linkedImage = edges.find(edge => edge.node.childImageSharp.sizes.originalImg.includes(kebabCase(post.frontmatter.title)));
-    return (
-        <BlogPostTemplate
-            content={post.html}
-            contentComponent={HTMLContent}
-            siteTitle={data.site.siteMetadata.title}
-            frontmatter={post.frontmatter}
-            images={linkedImage ? linkedImage.node.childImageSharp : null}
-        />
-    )
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post, allFile: { edges } } = data;
+  const linkedImage = edges.find(edge => edge.node.childImageSharp.sizes.originalImg.includes(kebabCase(post.frontmatter.title)));
+  return (
+    <BlogPostTemplate
+      content={post.html}
+      contentComponent={HTMLContent}
+      siteTitle={data.site.siteMetadata.title}
+      frontmatter={post.frontmatter}
+      images={linkedImage ? linkedImage.node.childImageSharp : null}
+    />
+  )
 }
 
 BlogPost.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.object,
-    })
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  })
 }
 
 export default BlogPost;
 
 export const pageQuery = graphql`
-    query ImagePageAndBlogPostByID($id: String!) {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-        markdownRemark(id: { eq: $id }) {
-            id
-            html
-            frontmatter {
-                date
-                formattedDate : date(formatString: "MMMM DD, YYYY")
-                title
-                description
-                tags
-                caption
-            }
-        }
-        allFile(filter : {extension : {eq : "png"}}) {
-            edges {
-                node {
-                    childImageSharp {
-                        sizes {
-                            originalImg
-                            ...GatsbyImageSharpSizes
-                        }
-                    }
-                }
-            }
-        }
+  query ImagePageAndBlogPostByID($id: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+  }
+  markdownRemark(id: { eq: $id }) {
+    id
+    html
+    frontmatter {
+      date
+      formattedDate : date(formatString: "MMMM DD, YYYY")
+      title
+      description
+      tags
+      caption
     }
+  }
+  allFile(filter : {extension : {eq : "png"}}) {
+    edges {
+        node {
+          childImageSharp {
+            sizes {
+              originalImg
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
+      }
+    }
+  }
 `
