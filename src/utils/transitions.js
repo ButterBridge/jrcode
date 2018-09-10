a3e46c8bf3475af8b872740217a18b637085c5dc
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-09-10 07:44:31 +0100

    adapt 404 page

--- src/utils/transitions.js
+++ src/utils/transitions.js
@@ -1,127 +1,127 @@
 const getTransitionStyles = timeout => ({
-    fade : {
-        entering: {
-            opacity: 0,
-        },
-        entered: {
-            transition: `opacity ${timeout}ms ease-in-out`,
-            opacity: 1,
-        },
-        exited: {
-            transition: `opacity ${timeout}ms ease-in-out`,
-            opacity: 0,
-        }, 
+  fade: {
+    entering: {
+      opacity: 0,
     },
-
-    fadeOut : {
-        entering: {
-            opacity: 1,
-        },
-        entered: {
-            transition: `opacity ${timeout}ms ease-in-out`,
-            opacity: 0,
-        },
-        exited: {
-            transition: `opacity ${timeout}ms ease-in-out`,
-            opacity: 0,
-        }, 
+    entered: {
+      transition: `opacity ${timeout}ms ease-in-out`,
+      opacity: 1,
     },
-
-    slide : {
-        entering: {
-            transform: 'translateX(-100%)'
-        },
-        entered: {
-            transition: `transform ${timeout}ms`,
-            transform: 'translateX(0%)'
-        },
-        exited: {
-            transform: 'translateX(0%)'
-        }
+    exited: {
+      transition: `opacity ${timeout}ms ease-in-out`,
+      opacity: 0,
     },
+  },
 
-    slideOut : {
-        entering: {
-            transform: 'translateX(0%)'
-        },
-        entered: {
-            transition: `transform ${timeout}ms`,
-            transform: 'translateX(-100%)'
-        },
-        exited: {
-            transform: 'translateX(-100%)'
-        }
+  fadeOut: {
+    entering: {
+      opacity: 1,
     },
-
-    descend : {
-        entering: {
-            transform: 'translateY(-100%)'
-        },
-        entered: {
-            transition: `transform ${timeout}ms`,
-            transform: 'translateY(0%)'
-        },
-        exited: {
-            transform: 'translateY(0%)'
-        }
+    entered: {
+      transition: `opacity ${timeout}ms ease-in-out`,
+      opacity: 0,
     },
-
-    dip : {
-        entering: {
-            transform: 'translateY(0%)'
-        },
-        entered: {
-            transition: `transform ${timeout}ms`,
-            transform: 'translateY(4em)'
-        },
-        exited: {
-            transform: 'translateY(0%)'
-        }
+    exited: {
+      transition: `opacity ${timeout}ms ease-in-out`,
+      opacity: 0,
     },
+  },
 
-    ascend : {
-        entering: {
-            transform: 'translateY(0%)'
-        },
-        entered: {
-            transition: `transform ${timeout}ms`,
-            transform: 'translateY(-100%)'
-        },
-        exited: {
-            transform: 'translateY(-100%)'
-        }
+  slide: {
+    entering: {
+      transform: 'translateX(-100%)'
     },
-
-    undip : {
-        entering: {
-            transform: 'translateY(4em)'
-        },
-        entered: {
-            transition: `transform ${timeout}ms`,
-            transform: 'translateY(0%)'
-        },
-        exited: {
-            transform: 'translateY(0%)'
-        }
+    entered: {
+      transition: `transform ${timeout}ms`,
+      transform: 'translateX(0%)'
+    },
+    exited: {
+      transform: 'translateX(0%)'
     }
+  },
+
+  slideOut: {
+    entering: {
+      transform: 'translateX(0%)'
+    },
+    entered: {
+      transition: `transform ${timeout}ms`,
+      transform: 'translateX(-100%)'
+    },
+    exited: {
+      transform: 'translateX(-100%)'
+    }
+  },
+
+  descend: {
+    entering: {
+      transform: 'translateY(-100%)'
+    },
+    entered: {
+      transition: `transform ${timeout}ms`,
+      transform: 'translateY(0%)'
+    },
+    exited: {
+      transform: 'translateY(0%)'
+    }
+  },
+
+  dip: {
+    entering: {
+      transform: 'translateY(0%)'
+    },
+    entered: {
+      transition: `transform ${timeout}ms`,
+      transform: 'translateY(4em)'
+    },
+    exited: {
+      transform: 'translateY(0%)'
+    }
+  },
+
+  ascend: {
+    entering: {
+      transform: 'translateY(0%)'
+    },
+    entered: {
+      transition: `transform ${timeout}ms`,
+      transform: 'translateY(-100%)'
+    },
+    exited: {
+      transform: 'translateY(-100%)'
+    }
+  },
+
+  undip: {
+    entering: {
+      transform: 'translateY(4em)'
+    },
+    entered: {
+      transition: `transform ${timeout}ms`,
+      transform: 'translateY(0%)'
+    },
+    exited: {
+      transform: 'translateY(0%)'
+    }
+  }
 })
 
 const composeTransitionStyles = ({ timeout, status, actions }) => {
-    const {elements, transitions} = actions.reduce((acc, action) => {
-        acc.elements = {
-            ...acc.elements,
-            ...getTransitionStyles()[action][status]
-        };
-        acc.transitions.push(getTransitionStyles(timeout)[action][status].transition || '');
-        return acc;
-    }, {
-        elements: {},
-        transitions : []
-    });
-    
-    elements.transition = transitions.join();
+  const { elements, transitions } = actions.reduce((acc, action) => {
+    acc.elements = {
+      ...acc.elements,
+      ...getTransitionStyles()[action][status]
+    };
+    acc.transitions.push(getTransitionStyles(timeout)[action][status].transition || '');
+    return acc;
+  }, {
+    elements: {},
+    transitions: []
+  });
 
-    return elements;
+  elements.transition = transitions.join();
+
+  return elements;
 }
-    
+
 export default composeTransitionStyles;
