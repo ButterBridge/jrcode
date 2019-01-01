b6b2af2345461c8df92107c52b90167a6cad1c6f
ButterBridge <jonnyrathbone@gmail.com>
2019-01-01 00:35:33 +0000

    add some definitions

---
+++ src/data/definitions.js
@@ -0,0 +1,37 @@
+export default {
+  CMS: {
+    desc:
+      "Content Management System - a way of abstracting and delivering the management of content / data for a user, most likely so they do not have to interface directly with a database or perhaps a back-end entirely",
+    aka: ["content management system", "content management"]
+  },
+  gatsby: {
+    desc:
+      'Gatsby is a tool for creating static sites with React. It pre-renders the html before sending it to the client, but the code is split in such a way that allows you to create dynamic content with React too. The result is, in their words, "blazing fast" site navigation.',
+    aka: ["gatsby's"],
+    link: ""
+  },
+  jrcode: {
+    desc: "This site. Welcome!",
+    aka: ["this site"],
+    link: ""
+  },
+  netlify: {
+    desc: "A suite of tools for hosting websites, including this one!",
+    aka: ["netlify's"],
+    link: ""
+  },
+  refactoring: {
+    desc:
+      "The process of going over working code to improve it, perhaps to make it more maintainable, readable, or to modernise it.",
+    aka: ["refactors", "refactored", "refactor"]
+  },
+  "styled components": {
+    desc:
+      "A library designed to take advantage of React's ability to compose it's components so that the user can integrate styling alongside their application code."
+  },
+  styling: {
+    desc:
+      "Coding the layout, theme, aethetics and other user experiences of a website or application.",
+    aka: ["styled", "styles", "style"]
+  }
+};
